62bd2c9ca35f40b5f560d0ff468e17cd99996234
DevHoliday <devholidaydev@gmail.com>
2023-09-24 03:25:24 +0300

    added account menu in header

---
+++ src/components/account.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import { useEffect, useRef, useState } from "react"
+import { createPortal } from "react-dom"
+import styles from '@/styles/header.module.css'
+import Image from "next/image"
+import Link from "next/link"
+
+export function Account() {
+    const divAccountRef = useRef<null|HTMLDivElement>(null);
+    const divAccountMenuRef = useRef<null|HTMLDivElement>(null);
+    const [showAccountMenu, setShowAccountMenu] = useState<boolean>(false);
+
+    useEffect(() => {
+        const closeDropDown = (event: Event) => {
+            if (!divAccountRef.current) {
+                return;
+            }
+
+            if (!event.composedPath().includes(divAccountRef.current as HTMLDivElement) && 
+                !event.composedPath().includes(divAccountMenuRef.current as HTMLDivElement)) {
+                setShowAccountMenu(false);
+            }
+        };
+
+        window.addEventListener('click', closeDropDown);
+
+        return () => window.removeEventListener('click', closeDropDown);
+    }, []);
+
+    return (
+        <>
+            <div ref={divAccountRef} className={styles.account} onClick={() => setShowAccountMenu((prevState: any) => !prevState)}>
+                <Image width={26} height={26} src='/icons/account.svg' alt='' />
+            </div>
+
+            {showAccountMenu && createPortal(
+                <div ref={divAccountMenuRef} className={styles.accountMenu}>
+                     <ul>
+                        <li><Link href={`${process.env.NEXT_PUBLIC_URL_ACCOUNT as string}/logout`}>Log out</Link></li>
+                    </ul>
+                </div>,
+                document.body
+            )}
+        </>
+    )
+}
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,20 @@
 import Link from "next/link"
 import styles from '@/styles/header.module.css'
 import Image from "next/image"
+import { Account } from "./account";
 
 export function Header() {
     return (
-        <div className={styles.logo}>
-            <Link href='/'>
-                <Image width={26} height={26} src='/logo.svg' alt='' />
-                <span className={styles.projectName}>AppFrame</span>
-            </Link>
-        </div>
+        <>
+            <div className={styles.wrapper}>
+                <div className={styles.logo}>
+                    <Link href='/'>
+                        <Image width={26} height={26} src='/logo.svg' alt='' />
+                        <span className={styles.projectName}>AppFrame</span>
+                    </Link>
+                </div>
+                <Account />
+            </div>
+        </>  
     )
 }
